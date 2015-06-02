b76abaaad0a913ceeea13b7e6d177eb54f9d358d
TCYRUS <tcyrus@users.noreply.github.com>
2015-06-02 09:21:24 -0400

    Separated Main Activity and SSO Activity

--- app/src/main/java/me/tcyrus/iodinessoandroid/SSOActivity.java
+++ app/src/main/java/me/tcyrus/iodinessoandroid/SSOActivity.java
@@ -1,48 +1,27 @@
 package me.tcyrus.iodinessoandroid;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
-import java.net.URL;
+public class SSOActivity extends ActionBarActivity {
 
-import edu.tjhsst.tcyrus.iodine.SSO;
-
-public class MainActivity extends ActionBarActivity {
-	public final static String SSO_MESSAGE = "me.tcyrus.iodinessoandroid.SSOToken";
 	WebView webView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_sso);
+		Intent intent = getIntent();
+		String sso = intent.getStringExtra(MainActivity.SSO_MESSAGE);
 		try {
-			String url = SSO.SSOUrl(getString(R.string.app_name), new URL("http://1.1.1.1")).toString();
-			Log.d("URL",url);
+			String url = "https://iodine.tjhsst.edu/ajax/sso/valid_key?sso="+sso;
+			Log.d("URL", url);
 			webView = (WebView) findViewById(R.id.webView);
-			webView.getSettings().setJavaScriptEnabled(true);
-			webView.setWebViewClient(new WebViewClient(){
-				@Override
-				public boolean shouldOverrideUrlLoading(WebView view, String url) {
-					try {
-						if (url.contains("1.1.1.1")) {
-							String sso= Uri.parse(url).getQueryParameter("sso");
-							Log.d("SSO Token",sso);
-							Intent intent=new Intent(MainActivity.this,SSOActivity.class);
-							intent.putExtra(SSO_MESSAGE,sso);
-							startActivity(intent);
-							return true;
-						}
-					} catch (Exception e) {return false;}
-					return false;
-				}
-			});
 			webView.loadUrl(url);
 		} catch (Exception e) {Log.d("Exception",e.toString());}
 	}
@@ -50,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
+		getMenuInflater().inflate(R.menu.menu_sso, menu);
 		return true;
 	}
 
